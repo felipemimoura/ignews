73a3eb31b54a4edf710eaebecac68110ada611dc
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-19 07:57:25 -0300

    feat: criação do componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import styles from "./style.module.scss"
+
+export const Header = () => {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src="/images/logo.svg" alt="Logo ignews" width="110" height="31" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Post</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
