d959b12b80df435760d925156096c7fbe44193c2
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-19 16:48:31 -0300

    feat: adicona propriedade de id do produto para o botão de subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+    productId: string
+}
+export function SubscribeButton({ productId }: SubscribeButtonProps) {
     return (
         <button type="button" className={styles.subscribeButton}>
             Subscribe now
