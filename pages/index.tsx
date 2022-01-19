4438fb247d496d1c3bb56c6e56abe33d5de0ed79
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-19 16:47:35 -0300

    feat: adicona botão para se inscriver e realiza chamadas de SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,59 @@
 
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+import { SubscribeButton } from '../src/components/SubscribeButton'
 import styles from './home.module.scss'
-export default function Home() {
+import { stripe } from '../services/stripe'
+
+
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number,
+
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
   return (
     <>
       <Head>
         <title>Home | IG.news</title>
       </Head>
       <main className={styles.contentContainer}>
+
         <section className={styles.hero}>
+
           <span>👏Hey, welcome</span>
+
           <h1>News about the <span>React</span> world.</h1>
-          <p>Get acesse to all the publications <br /> <span>by $9.90</span></p>
+
+          <p>Get acesse to all the publications <br /> <span>by {product.amount}</span></p>
+
+          <SubscribeButton productId={product.priceId} />
+
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KJjKqJqAGlDH9mtxNXSyqsZ')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product
+    }
+  }
+
 }
