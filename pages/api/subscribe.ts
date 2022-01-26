2a8dc4ba855a998c15cc5c0efb7b84b414992e17
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-26 08:51:14 -0300

    feat: iniciado a comunição com o stripe

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,63 @@
+import { query } from "faunadb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
+export default async (req: NextApiRequest, response: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const user = await fauna.query<User>(
+      query.Get(
+        query.Match(
+          query.Index("users_by_email"),
+          query.Casefold(session.user.email)
+        )
+      )
+    );
+    console.log(user);
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        query.Update(query.Ref(query.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1KJjKqJqAGlDH9mtxNXSyqsZ", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    response.setHeader("Allow", "Post");
+    response.status(405).end("Method not allowed");
+  }
+};
